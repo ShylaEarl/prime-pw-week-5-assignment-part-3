c1dba6088b85c5eb486ce6507e6d649336d07464
Shyla Earl <shyla.earl@gmail.com>
2021-03-24 17:42:27 -0500

    showCollection function working and logging with proper format

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -2,6 +2,7 @@ console.log('***** Music Collection *****')
 
 //created variable collection with a value of an empty array
 let collection = [];
+//globally scoped album variable that can be accessed outside of the function
 let album = {};
 
 //created addToCollection function which takes in 3 parameters
@@ -18,6 +19,7 @@ function addToCollection(title, artist, yearPublished){
   return album;
 }//end addToCollection function
 
+//testing addToCollection function by calling it and then logging the newly added object
 console.log(addToCollection('Girls Just Want to have Fun', 'Cyndi Lauper', 1984));
 console.log('I have added ', album, ' to my record collection!');
 // console.log('I have just added ', Object.values(album), ' to my record collection!');
@@ -42,4 +44,19 @@ console.log('I have added ', album, ' to my record collection!');
 console.log(addToCollection('Savior', 'Soft Kill', 2018));
 console.log('I have added ', album, ' to my record collection!');
 
+//testing collection array to make sure all album objects have been added to the collection array
 console.log('My record collection includes:', collection);
+
+//created showCollection function that accepts an array parameter
+function showCollection(array){
+  //logging number of items in the array
+  console.log('I have ', array.length, ' albums in my collection.');
+  //looping through the array to capture each album's info
+  for(let i = 0; i < array.length; i++){
+    //logging each album's info in specified format
+    console.log('They are:', `${array[i].title} by ${array[i].artist}, published in ${array[i].yearPublished}.`);
+  }//end for loop
+  // return `${array[i].title} by ${array[i].artist}, published in ${array[i].yearPublished}.`;
+}//end showCollection function
+
+console.log('Testing showCollection function', showCollection(collection));
